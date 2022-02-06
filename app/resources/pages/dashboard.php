100ed100a8201826ebc143c5ebb7188ead55306a
Jonel Zape <jonel.zape@serino.com>
2022-02-07 00:34:27 +0800

    add quick crate post button in dashboard

--- app/resources/pages/dashboard.php
+++ app/resources/pages/dashboard.php
@@ -3,5 +3,22 @@
         <ol class="breadcrumb">
             <li class="active">Dashboard</li>
         </ol>
+
+        <div class="row">
+            <div class="col-md-3 margin-bottom-15">
+                <div class="widget widget-net-income">
+                    <div class="widget-counter">
+                        <center>
+                        <a href="/posts/create">Create New Post</a>
+                        </center>
+
+                    </div>
+                    <div class="widget-label">
+                        Create & Publish
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </div>
